5dccd364b9f7f46a2d25afa66a5f4544e91cdb8b
Stimonis <simon.stiens@gmail.com>
2018-05-02 16:53:16 +0200

    add

---
+++ Mini-Chat/server.js
@@ -0,0 +1,5 @@
+// commencer par ajouter l'html
+// récupérer les posts et les ajouter dans ladb
+// afficher la db
+// demander le pseudo quand on se co
+// mettre un message quand qq un se connecte
